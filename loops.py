c863465a13f1a54eaedb1b236b4f89a2da283b71
Saumya Singh <singhsaumyait034@gmail.com>
2026-02-24 14:08:04 +0530

    If Else Loop

---
+++ loops.py
@@ -0,0 +1,9 @@
+greetings = "Good Morning"
+
+if greetings == "Morning":
+    print("condition matches")
+    print("Hey this is second line")
+else:
+    print("condition do not matches")
+    print("Hey this is third line")
+print("if else conditions is completed")
