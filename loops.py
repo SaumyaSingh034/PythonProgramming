4cbd4b70255c4ee5f177a7105a83051e770c00ca
Saumya Singh <singhsaumyait034@gmail.com>
2026-02-24 14:22:06 +0530

    For Loop

--- loops.py
+++ loops.py
@@ -7,3 +7,28 @@ else:
     print("condition do not matches")
     print("Hey this is third line")
 print("if else conditions is completed")
+
+
+
+#for loop
+obj = [2,3,4,5,6,7,8,9]
+
+for i in obj:
+    print(i*2)
+
+    # sum of first 5 natural numbers 1+2+3+4+5 = 15
+sum = 0;
+for x in range(1,6):
+    sum += x
+print("------------------------------------")
+print(sum)
+
+print("------------------------------------")
+#Jump 2 iterations
+for x in range(1,10,2):
+    print(x)
+
+print("------------------------------------")
+#Skipp first index
+for m in range(10):
+    print(m)
