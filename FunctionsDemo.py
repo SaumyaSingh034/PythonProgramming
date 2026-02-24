1d2eb0266ab38068c98088fd1ef0471ff4f5fe92
Saumya Singh <singhsaumyait034@gmail.com>
2026-02-24 14:40:45 +0530

    Function declarations

---
+++ FunctionsDemo.py
@@ -0,0 +1,15 @@
+#Function Declaration
+
+def greetme():
+    print("Good Morning")
+
+greetme()
+
+def greetme(name):
+    print("Good Evening "+name)
+
+greetme("Saumya Singh")
+
+def addme(x,y):
+    return x+y
+print(addme(3,4))
